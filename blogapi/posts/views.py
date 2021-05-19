fb0bf3dd75003d8a06e85de17ab7b8cf864bc06b
andreytp <andreytp@gmail.com>
2021-05-19 18:46:54 +0300

    ch5 done

---
+++ blogapi/posts/views.py
@@ -0,0 +1,13 @@
+from .models import Post
+from rest_framework import generics
+from .serializers import PostSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
